7ca8a14f15053c273be0f1e67f6e20f1312f44d7
Lars Haugan <lars@larshaugan.net>
2015-01-26 18:42:55 +0100

    Added configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+server = ''
+dbname = 'paste'
+
+sites = [{'archive':'http://pastebin.com/archive','raw':'http://pastebin.com/raw.php?i=%s'}]
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36'}
